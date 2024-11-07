dd240c04a48ffd95fbfe30a19e31fac0b597f6fe
DmitryFreeLance <dmitryshagaev@bk.ru>
2024-11-07 14:59:16 +0700

    start

---
+++ src/main/java/ru/linkphoria/linkphoriaproject/LinkPhoriaProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.linkphoria.linkphoriaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkPhoriaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkPhoriaProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/linkphoria/linkphoriaproject/LinkPhoriaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.linkphoria.linkphoriaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkPhoriaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
